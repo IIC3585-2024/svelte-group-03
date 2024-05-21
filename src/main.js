b19ac29d3c6879d309f5e67d5fff4cc4e6ee6a07
aplaza2 <aplaza2@uc.cl>
2024-05-21 14:01:07 -0400

    Add header & footer

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import './app.css'
 import App from './App.svelte'
+import Header from './Header.svelte'
+import Footer from './Footer.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
 })
 
+const header = new Header({
+  target: document.getElementById('header'),
+})
+
+const footer = new Footer({
+  target: document.getElementById('footer'),
+})
+
 export default app
+export { header, footer }
